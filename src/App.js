9ce6d6799c238b4d07884a4cbb69db076203eb3e
Zahar <zahratulnazirah@gmail.com>
2023-10-01 22:14:47 +0800

    creating functional component
    create new folder "component"
    create new file "Welcome.js"
    create function App()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome  from './components/Welcome'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is my first react project
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <Welcome />
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Welcome.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+// function Welcome(){
+//     return <h1>Good Day every one</h1>
+// }
+
+const Welcome = () => <h1>Hello Everybody</h1>
+
+export default Welcome
